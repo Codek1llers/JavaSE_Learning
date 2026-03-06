586730f33b6326e5191a6333481f8fb7cf6f4a14
zhangrui <zr184517@outlook.com>
2026-03-06 16:08:40 +0800

    十进制、八进制、二进制、十六进制表示

--- day01-basic-code/src/Main.java
+++ day01-basic-code/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        int a1 = 0B111110;
+        int a2 = 0123;
+        int a3 = 0X123;
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
     }
 }
